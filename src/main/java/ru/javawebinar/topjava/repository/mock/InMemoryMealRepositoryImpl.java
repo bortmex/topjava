14a9f36817dcb2c2f24f9f854288a8f193c9ad29
Rog <alexanderogov@gmail.ru>
2016-12-20 23:00:14 +0300

    HW2 middle edit

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.mock;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -21,6 +22,8 @@ import java.util.stream.Collectors;
  */
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryMealRepositoryImpl.class);
+
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
@@ -30,6 +33,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        LOG.info("save " + meal + "; userId " + userId);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
         }
@@ -41,6 +45,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     @Override
     public boolean delete(int id, int userId) {
         if (repository.containsKey(id) && repository.get(id).getUserId() == userId) {
+            LOG.info("delete " + id + "; userId " + userId);
             repository.remove(id);
             return true;
         } else throw new NotFoundException("Не найдено");
@@ -49,12 +54,14 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     @Override
     public Meal get(int id, int userId) {
         if (repository.containsKey(id) && repository.get(id).getUserId() == userId) {
+            LOG.info("get " + id + "; userId " + userId);
             return repository.get(id);
         } else throw new NotFoundException("Не найдено");
     }
 
     @Override
     public Collection<MealWithExceed> getAll(int userId) {
+        LOG.info("getAll");
         List<Meal> listMeal = repository.values().stream().filter(meal -> meal.getUserId() == userId).sorted((a, b) -> (-1) * a.getDateTime().compareTo(b.getDateTime())).collect(Collectors.toList());
         if (listMeal.size() == 0) return null;
         else return MealsUtil.getWithExceeded(listMeal , MealsUtil.DEFAULT_CALORIES_PER_DAY);
